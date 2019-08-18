626f98f2600db218ff1bbce4d065b96355730c7e
TheDoctorOne <mahmudkocas@gmail.com>
2019-08-18 04:05:58 +0300

    Initial Commit

---
+++ src/com/mahmudkocas/tuto/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.mahmudkocas.tuto;
+
+public class HelloWorld {
+	private String message;
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public void getMessage() {
+		System.out.println("MESSAGE : " + message);
+	}
+}
---
+++ src/com/mahmudkocas/tuto/Main.java
@@ -0,0 +1,17 @@
+package com.mahmudkocas.tuto;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	private static ApplicationContext ctx;
+
+	public static void main(String[] args) {
+		ctx = new ClassPathXmlApplicationContext("Beans.xml");
+		HelloWorld obj = (HelloWorld) ctx.getBean("helloWorld");
+		obj.getMessage();
+
+	}
+
+}
